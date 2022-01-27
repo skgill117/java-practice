61c608dae5ccd0a7f2ac5990e7f29c63bf1c1d85
Shiv Kumar <shiv.kumar01@nagarro.com>
2022-01-27 10:44:22 +0530

    minor changes

--- dsa/src/main/java/com/dsa/LinkedList/LinkedList.java
+++ dsa/src/main/java/com/dsa/LinkedList/LinkedList.java
@@ -1,5 +1,12 @@
 package com.dsa.LinkedList;
 
+import java.util.HashSet;
+
+// rverse linkedlist, loop detection
+
+
+
+
 public class LinkedList {
 
 	static Node head;
@@ -14,6 +21,21 @@ public class LinkedList {
 			next = null;
 		}
 	}
+	
+	 private Node push(int new_data)
+    {
+        /* 1 & 2: Allocate the Node &
+                  Put in the data*/
+        Node new_node = new Node(new_data);
+ 
+        /* 3. Make next of new Node as head */
+        new_node.next = head;
+ 
+        /* 4. Move the head to point to new Node */
+        head = new_node;
+        
+        return head;
+    }
 
 	/* Function to reverse the linked list */
 	Node reverse(Node node) {
@@ -30,6 +52,29 @@ public class LinkedList {
 		
 		return prev;
 	}
+	
+	 // Returns true if there is a loop in linked
+    // list else returns false.
+    static boolean detectLoop(Node node)
+    {
+        HashSet<Node> set = new HashSet<Node>();
+        while (node != null) {
+            // If we have already has this node
+            // in hashmap it means their is a cycle
+            // (Because you we encountering the
+            // node second time).
+            if (set.contains(node))
+                return true;
+ 
+            // If we are seeing the node for
+            // the first time, insert it in hash
+            set.add(node);
+ 
+            node = node.next;
+        }
+ 
+        return false;
+    }
 
 	// prints content of double linked list
 	void printList(Node node) {
@@ -41,10 +86,11 @@ public class LinkedList {
 
 	public static void main(String[] args) {
 		LinkedList list = new LinkedList();
-		list.head = new Node(85);
-		list.head.next = new Node(15);
-		list.head.next.next = new Node(4);
-		list.head.next.next.next = new Node(20);
+		list.push(85);
+		list.push(15);
+		list.push(4);
+		list.push(20);
+		
 
 		System.out.println("Given Linked list");
 		list.printList(head);
@@ -52,6 +98,14 @@ public class LinkedList {
 		System.out.println("");
 		System.out.println("Reversed linked list ");
 		list.printList(head);
+		System.out.println("\n\n");
+		
+		/*Create loop for testing */
+		list.head.next.next.next.next = list.head;
+		if (detectLoop(head))
+            System.out.println("Loop found");
+        else
+            System.out.println("No Loop");
 
 	}
 
--- problems/src/main/java/com/practice/maths/Exponential.java
+++ problems/src/main/java/com/practice/maths/Exponential.java
@@ -6,13 +6,13 @@ public class Exponential {
 
 		double d = 1.01;
 		int exp = 365;
-		System.out.println(Math.pow(d, exp));
 		double result = 1;
 		for (int i = 1; i <= 365; i++) {
 			result = result * d;
 
 		}
 		System.out.println(result);
+		System.out.println(Math.pow(d, exp));
 	}
 
 }
